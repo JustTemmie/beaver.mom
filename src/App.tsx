b5261c0ea01b60b0623d7c05a1a00af0227161ea
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2023-11-17 12:55:13 +0100

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function test() : JSX.Element {
-  return <h5>:3</h5>
-}
+const Wrapper = styled.div`
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background-color: #333;
+  color: #fff;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-function App() {
+const Title = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 30px;
+`;
+
+const Content = styled.div`
+  margin-top: 20px;
+`;
+
+const Footer = styled.footer`
+  margin-top: 50px;
+  font-size: 0.8rem;
+`;
+
+// Define a functional component for the homepage
+const HomePage: React.FC = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          hi guys
-        </h2>
-        <p>
-          was poppin?
-        </p>
-        <div>
-          {test()}
-        </div>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>hiiiiiiiiii</Title>
+      <Description>welcome to beaver.mom</Description>
+      <Content className="content">
+        {/* Your content here */}
+        <p>this is real content</p>
+        <p>anything you've seen before is simply fake; it wasn't real</p>
+      </Content>
+      <Footer>
+        <p>Thanks for visiting!</p>
+        <p>&copy; 2023 Beaver.mom</p>
+      </Footer>
+    </Wrapper>
   );
-}
+};
 
-export default App;
+export default HomePage;
